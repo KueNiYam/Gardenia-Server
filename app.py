ee3ed5eae17bd26022844be2fa5f7ec6f233e740
Kuenhwi <kueniyam@naver.com>
2019-05-25 13:59:42 +0900

    UPDATE Gardenia-Server
    코드를 윈도우로 옮기기 위해 잠깐 커밋

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+#from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+#socketio = SocketIO(app)
+
+@app.route('/')
+def ping():
+    return 'ping'
+
