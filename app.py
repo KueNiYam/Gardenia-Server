d3c3d1ea84fbc840903962b473d30e171b101879
Kuenhwi <kueniyam@naver.com>
2019-06-01 16:28:05 +0900

    UPDATE client-test & app
    드디어 연결됩니다.git add *

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@app.route('/ping')
-def ping():
-    socketio.emit('ping event', {'msg': 'pong'})
-    return 'pong' 
+@socketio.on('message')
+def handle_message(json):
+    print('received message: ' + json['data'])
+
+@socketio.on('connect')
+def test_connect():
+    socketio.emit('response', {'data': 'Connected'})
+    print('connected')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0')
